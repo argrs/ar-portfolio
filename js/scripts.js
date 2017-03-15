6dac3639fdef5588f53f002a13cdb9e04dcb19dc
Amy Rogers <amy.rogers@live.ca>
2017-03-15 16:26:30 -0400

    more changes

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,3 @@ $(window).scroll(function() {
   }
 
 });
-
-
-$(function(){
-  $.scrollUp();
-  scrollName: 'scrollUp';
-  topDistance: '300';
-  topSpeed: 300;
-  animation: 'fade';
-  animationInSpeed: 200;
-  animationOutSpeed: 200;
-  activeOverlay: false
-});
